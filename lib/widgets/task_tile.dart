a9e10b92adf7638ec8360b493cb80e21f8df21ef
David <davidpineda.co@gmail.com>
2021-02-23 16:36:21 +0000

    use setState to check the tasks

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,24 +1,54 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   TaskTile({
     @required this.label,
   });
 
   final String label;
+
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
   bool isChecked = false;
+
+  void checkboxCallback(bool value) {
+    setState(() {
+      isChecked = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(label),
-      trailing: Checkbox(
-        onChanged: (value) {
-          isChecked = !isChecked;
-          print(isChecked);
-        },
-        value: isChecked,
+      title: Text(
+        widget.label,
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckbox(
+        checkBoxState: isChecked,
+        toogleCheckboxState: checkboxCallback,
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function toogleCheckboxState;
+
+  TaskCheckbox({this.checkBoxState, this.toogleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkBoxState,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: toogleCheckboxState,
     );
   }
 }
